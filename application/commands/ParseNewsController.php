fab566d62b4878d11db5a67ba8c827cbbb9d84dc
kikiyaki <krl9628@gmail.com>
2021-04-11 23:51:19 +0300

    create parser

---
+++ application/commands/ParseNewsController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ParseNewsController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     * @return int Exit code
+     */
+    public function actionIndex()
+    {
+        $url = 'https://api.corr.life/public/sections/5e01383bf4352e43d960b258/posts?after=1617621518036';
+        $newsData = file_get_contents($url);
+        $newsData = json_decode($newsData);
+        var_dump($newsData);
+        return ExitCode::OK;
+    }
+}
