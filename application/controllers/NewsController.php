76bae5f1bec832d2660a128229ee424dee5fe4e2
kikiyaki <krl9628@gmail.com>
2021-04-12 03:25:44 +0300

    default index action overriding, limit, offset

--- application/controllers/NewsController.php
+++ application/controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\models\News;
 use yii\rest\ActiveController;
+use yii\web\Request;
 
 /**
  * Class NewsController
@@ -14,8 +15,21 @@ class NewsController extends ActiveController
 {
     public $modelClass = 'app\models\News';
 
-    public function actionIndex()
+    public function actionIndex(Request $request)
     {
-        return News::find()->all();
+        $limit = $request->get('limit', null);
+        $offset = $request->get('offset', 0);
+        return News::find()
+            ->limit($limit)
+            ->offset($offset)
+            ->all();
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        //default index action overriding
+        unset($actions['index']);
+        return $actions;
     }
 }
